19e8510ecdfa36147074e1812829ed9a6313e619
Afiq Leo <afiqleo07@gmail.com>
2026-06-08 22:31:35 +0800

    Testing

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
+        System.out.println("=== BIASAKAN YANG BETUL, BETULKAN YANG BIASA ===");
+        System.out.println("--- Sistem Reservasi Hotel Mu ---");
+        System.out.print("Masukkan nama anda untuk tempahan: ");
+        String namaInput = input.nextLine();
+
+        // Di sini kita akan panggil Class yang kawan anda buat nanti
+        Customer pelanggan = new Customer(namaInput);
+        
+        System.out.println("\n--- Status Tempahan Berjaya! ---");
+        // Memanggil fungsi paparan dari class kawan anda
+        pelanggan.paparInfo(); 
+        
+        input.close();
     }
 }
